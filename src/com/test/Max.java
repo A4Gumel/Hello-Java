6b047ec8ef608fcfeb8d3d5f7d068fb02f8a2ae1
auwal <a4auwal@gmail.com>
2022-07-07 17:40:56 +0100

    max in range

--- src/com/test/Max.java
+++ src/com/test/Max.java
@@ -2,9 +2,10 @@ package com.test;
 
 public class Max {
     public static void main(String[] args) {
-        int[] arr = {8, 9, 10, 12, 13, 14, 15};
+        int[] arr = {12, 13, 14, 18, 9, 10, 5};
 
         System.out.println(max(arr));
+        System.out.println(maxInRange(arr, 4, arr.length - 1));
     }
 
     static int max(int[] arr) {
@@ -15,6 +16,21 @@ public class Max {
             if (i > maxVal) {
 
                 maxVal = i;
+            }
+        }
+
+        return maxVal;
+    }
+
+    static int maxInRange(int[] arr, int start, int end) {
+        int maxVal = arr[start];
+
+
+        for (int num = start; num <= end; num++) {
+
+            if (arr[num] > maxVal) {
+
+                maxVal = arr[num];
             }
         }
 

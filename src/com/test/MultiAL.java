e3e5e116e502e5c356f5ae3acf7ee7069eded2be
auwal <a4auwal@gmail.com>
2022-07-07 17:19:01 +0100

    MultiAL

---
+++ src/com/test/MultiAL.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiAL {
+
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+
+        // Initialization
+        for (int i = 0; i < 4; i++) { // for loop with the number of rows - 1 as condition
+            lists.add(new ArrayList<>());
+        }
+
+
+        // Add elements
+        for (int row = 0; row < 4; row++) {
+
+            for (int column = 0; column < 4; column++) {
+
+                lists.get(column).add(input.nextInt());
+            }
+        }
+
+    }
+}

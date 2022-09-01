bd7e577a2a3cb80f07791904737b91310e65523d
auwal <a4auwal@gmail.com>
2022-09-01 18:22:16 +0100

    start bubble sort

--- src/com/test/BubbleSort/BubbleSort.java
+++ src/com/test/BubbleSort/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 
-        int[] arr = {6, 5, 4, 3, 7, 2, 1, 8};
+        int[] arr = {6, 5, 4, 3, 7, 2, 1, 8, 0, };
         bubble(arr);
         System.out.println(Arrays.toString(arr));
 
